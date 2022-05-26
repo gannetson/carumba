df71e537413d55669ec9819474580bc4338adf20
Loek van Gent <loek@goodup.com>
2022-05-26 14:44:31 +0200

    Add initial

---
+++ home/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+
+from wagtail.core.models import Page
+
+
+class HomePage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
+
+class DefaultPage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
