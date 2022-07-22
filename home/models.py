38aafaccfddabc36a541760b3e21c7f68913991a
Loek van Gent <loek@goodup.com>
2022-07-22 12:35:49 +0200

    Improve gigs page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,6 @@
-from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel
-from wagtail.core.fields import RichTextField
-
+from wagtail.core.fields import StreamField, RichTextField
+from wagtail.core import blocks
 from wagtail.core.models import Page
 
 
@@ -15,6 +14,31 @@ class HomePage(Page):
 class DefaultPage(Page):
     body = RichTextField(blank=True)
 
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
+
+
+class GigBlock(blocks.StructBlock):
+    title = blocks.CharBlock()
+    date = blocks.DateBlock()
+    link = blocks.URLBlock(required=False)
+    description = blocks.RichTextBlock(required=False)
+    private = blocks.BooleanBlock(default=False, required=False)
+
+    class Meta:
+        icon = 'user'
+
+    def __str__(self):
+        return "{} - {}".format(self.date, self.title)
+
+
+class GigsPage(Page):
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('gig', GigBlock()),
+    ])
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
     ]
